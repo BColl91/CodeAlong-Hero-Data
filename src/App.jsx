b136d1fb631071c65e58e419bbc44361e5eb9867
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-25 21:09:02 +0100

    to check recording and fix errors

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import './App.css';
+import {allHeroes} from "./HeroData";
+ 
+const App = () => {
+  const [favourites, setFavourites] = useState([]);
+ 
+  const handleAdfdToFav = (hero) => {
+    let newFavArr = [favourites];
+    newFavArr.push(hero);
+    setFavourites(newFavArr);
+  }
+
+  return (
+    <>
+      <h1>Superhero Wiki</h1>
+ 
+      <div>
+        <h3>Favourite Heroes</h3>
+        {favourites.map((favHero, index) => {
+          return <Favourites key={index} favHeroData={favHero}/>
+        })}
+      </div>
+ 
+      {allHeroes.map((heroInfo, index) => {
+        return <HeroCard key={index} heroObj={heroInfo} favFunc={handleAddToFav}/>
+      })}
+    </>
+  );
+};
+ 
+const Favourites = (props) => {
+  return(
+    <div>
+      <p>Fav Hero</p>
+    </div>
+  )
+}
+ 
+const HeroCard = (props) => {
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      <p>HERO: {props.heroObj.hero}</p>
+ 
+      {show && (
+        <div>
+          <p>INFO: {props.heroObj.info}</p>
+          <p>VILLAIN: {props.heroObj.villain}</p>
+        </div>
+      )}
+ 
+      <button onClick={() => setShow(!show)}>{show ? "Hide Info" : "Show Info"}</button>
+      <button>Add to favourites</button>
+    </>
+  );
+};
+ 
+export default App;
